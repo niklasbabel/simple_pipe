671a57a03d16940160534996d59e724ec099d090
Niklas Babel <niklas@wane.co>
2016-12-05 14:26:38 +0100

    init

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+class SubdomainPresent
+  def self.matches?(request)
+    request.subdomain.present?
+  end
+end
+
+class SubdomainBlank
+  def self.matches?(request)
+    request.subdomain.blank?
+  end
+end
+
+Rails.application.routes.draw do
+  resources :contacts
+  constraints(SubdomainPresent) do
+    root 'contacts#index', as: :subdomain_root
+    devise_for :users
+    resources :users, only: :index
+  end
+
+  constraints(SubdomainBlank) do
+    root "welcome#index"
+    resources :accounts, only: [:new, :create]
+  end
+end
